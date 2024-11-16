9997a813c65a8895f6bf6f632155cb48310155f5
AdithyaPavan <136152474+AdithyaPavan@users.noreply.github.com>
2024-11-16 15:18:25 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/SampleMaven/App.java
@@ -0,0 +1,13 @@
+package com.example.demo.SampleMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Pavan Adithya - 22BD1A053V" );
+    }
+}
